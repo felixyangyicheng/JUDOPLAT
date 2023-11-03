208a0837e9416a8bcd9544e51b940393f21da992
felixyangyicheng <felix.yang.yicheng@gmail.com>
2023-11-03 06:56:38 +0100

    simple model

---
+++ Shared/Dtos/Svgs/PdfModelSimple.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace JUDOPLAT.Shared.Dtos.Svgs
+{
+    public class PdfModelSimple
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FileName { get; set; }
+        public string Category { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public DateTime DateOfUpdate { get; set; }
+    }
+}
